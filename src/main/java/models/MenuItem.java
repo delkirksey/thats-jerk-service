9a9de84a95052e158a58d894a8f373eb38340a6b
Del Kirksey <delmkirksey@gmail.com>
2017-11-29 10:41:13 -0500

    added menu item class

---
+++ src/main/java/models/MenuItem.java
@@ -0,0 +1,69 @@
+package models;
+
+public class MenuItem {
+
+    private long id;
+
+    private String name;
+
+    private String idescription;
+
+    private Double price;
+
+    private Double minPrice;
+
+    private Double maxPrice;
+
+    public MenuItem() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdescription() {
+        return idescription;
+    }
+
+    public void setIdescription(String idescription) {
+        this.idescription = idescription;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getMinPrice() {
+        return minPrice;
+    }
+
+    public void setMinPrice(Double minPrice) {
+        this.minPrice = minPrice;
+    }
+
+    public Double getMaxPrice() {
+        return maxPrice;
+    }
+
+    public void setMaxPrice(Double maxPrice) {
+        this.maxPrice = maxPrice;
+    }
+
+
+}
