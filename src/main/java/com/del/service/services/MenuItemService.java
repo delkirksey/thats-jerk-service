14a60130cae22a54e5bc5855f9441b7aa863f787
Del Kirksey <delmkirksey@gmail.com>
2017-11-29 17:49:44 -0500

    added a MenuItemService to handle calls from the MenuItemController

--- src/main/java/com/del/service/services/MenuItemService.java
+++ src/main/java/com/del/service/services/MenuItemService.java
@@ -1,4 +1,54 @@
 package com.del.service.services;
 
+import com.del.service.models.MenuItem;
+import com.del.service.repositories.MenuItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Service
 public class MenuItemService {
+
+    public final MenuItemRepository menuItemRepository;
+
+    @Autowired
+    public MenuItemService(MenuItemRepository menuItemRepository) {
+        this.menuItemRepository = menuItemRepository;
+    }
+
+    public List<MenuItem> getMenuItems(String menu) {
+        if (!menu.isEmpty()) {
+
+            return menuItemRepository.getAllByMenu(menu);
+        }
+
+        return menuItemRepository.findAll();
+    }
+
+    public void save(MenuItem menuItem) {
+        menuItemRepository.save(menuItem);
+    }
+
+    public void delete(Long id) {
+        menuItemRepository.delete(id);
+    }
+
+    public void update(Long id, MenuItem menuItem) {
+
+        MenuItem itemToUpdate = menuItemRepository.findOne(id);
+
+        if (itemToUpdate == null) throw new EntityNotFoundException();
+
+        itemToUpdate.setName(menuItem.getName());
+        itemToUpdate.setDescription(menuItem.getDescription());
+        itemToUpdate.setPrice(menuItem.getPrice());
+        itemToUpdate.setType(menuItem.getType());
+        itemToUpdate.setMinPrice(menuItem.getMinPrice());
+        itemToUpdate.setMaxPrice(menuItem.getMaxPrice());
+        itemToUpdate.setMenu(menuItem.getMenu());
+
+        menuItemRepository.save(itemToUpdate);
+    }
 }
