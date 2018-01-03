7af8d4b3d9d5819032fb49d4a103af5107e12095
Del Kirksey <delmkirksey@gmail.com>
2018-01-03 16:52:35 -0500

    created a Yelp Controller to allow the app to communicate with Yelp indirectly

---
+++ src/main/java/com/del/service/controllers/YelpController.java
@@ -0,0 +1,47 @@
+package com.del.service.controllers;
+
+import com.del.service.models.Menu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@RestController
+public class YelpController {
+
+    @Autowired
+    public YelpController() {
+        
+    }
+
+    @Autowired
+    private Environment env;
+
+    @ModelAttribute
+    public void setCORSResponseHeader(HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+    }
+
+    @GetMapping(value = "/yelpreviews", produces = "application/json")
+    public String getYelpReviews() {
+        final String uri = "https://api.yelp.com/v3/businesses/just-jerks-enas-jamaican-grill-loveland/reviews";
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("authorization", env.getProperty("yelp.api.key"));
+        
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+        String result = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class).getBody();
+
+        return result;
+    }
+}
